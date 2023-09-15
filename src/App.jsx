be41d833082cd27272cf12ae2350678c8c9d8cf3
fahadhos <generalus159@gmail.com>
2023-09-15 11:35:22 +0600

    Added: created function for cart ,alert for already purchased course,set credit hour limit upto 20,declared state for creditHour and CourseTitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+ 
 import './App.css'
+import Header from './components/Header/Header'
+import Card from './components/Card/Card'
+import Cards from './components/Cards/Cards'
+import Cart from './components/Cart/Cart'
+import Carts from './components/Carts/Carts'
+import Alert from './components/Alert/Alert'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() { 
+
+
+
+  const [ifExist, setIfExist] = useState(false);   
+  // course title dhorbo
+const [courseTitle, setCourseTitle]=useState([])
+// credit hour jog kortesi
+const [creditHour, setCreditHour] = useState(0)
+// for remaining credits hisab korbo
+// onlcick function ta
+  const handleAddCourse = (id,coursetitle,credithour)=>{
+
+    // console.log('Adding',creditHour);
+    
+  // console.log('newcourseTitle',newcourseTitle)
+  // console.log(courseTitle,id);
+  
+  const ifExist = courseTitle.includes(coursetitle);
+  // console.log(ifExist,'ifexist')
+ 
+  if(ifExist) {
+       setIfExist(true)
+    //  alert('Already added to cart')
+  
+  }
+  else{
+    // const newcourseTitle=  Array.from(new Set([...courseTitle,coursetitle]))
+    const newcourseTitle=   [...courseTitle,coursetitle]
+    setCourseTitle(newcourseTitle)
+
+    const newCredithour = creditHour+credithour
+
+    if(newCredithour>20){
+      return alert('credit hour can not exceed from 20')
+      // setwarningforch(true)
+    }else{
+
+      setCreditHour(newCredithour);
+    }
+    }
+    // console.log("New creditHour:", newCredithour);
+    // setIfExist(true);
+
+    // Automatically hide the Alert after 3 seconds
+    setTimeout(() => {
+      setIfExist(false);
+    }, 3000);
+    // console.log(newcourseTitle)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <><Header></Header>
+    <div className='flex mx-auto'>
+      <Cards      handleAddCourse={handleAddCourse}
+            ></Cards>
+         
+      <Carts 
+    ifExist={ifExist}  courseTitle={courseTitle} creditHour={creditHour}></Carts>
+    </div></>
   )
 }
 
