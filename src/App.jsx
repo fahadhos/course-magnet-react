75f2e9e7e3dc3cd75c0d0bfcb9b5cfa4bffe285d
fahadhos <generalus159@gmail.com>
2023-09-15 22:46:54 +0600

    Modified: some bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,102 @@
-import { useState } from 'react'
+import {  useState } from 'react'
  
 import './App.css'
-import Header from './components/Header/Header'
-// import Card from './components/Card/Card'
-import Cards from './components/Cards/Cards'
-// import Cart from './components/Cart/Cart'
-import Carts from './components/Carts/Carts'
-// import Alert from './components/Alert/Alert'
+import Header from './components/Header/Header' 
+import Cards from './components/Cards/Cards' 
+import Carts from './components/Carts/Carts' 
 
+import SweetAlert2 from 'react-sweetalert2';
+ 
 
 function App() { 
-
-
-
+ 
+   
+  
+  const [swalProps, setSwalProps] = useState({});
   const [ifExist, setIfExist] = useState(false);   
   // course title dhorbo
-const [courseTitle, setCourseTitle]=useState([])
-// credit hour jog kortesi
-const [creditHour, setCreditHour] = useState(0)
-// for remaining credits hisab korbo
-// onlcick function ta
-  const handleAddCourse = (id,coursetitle,credithour)=>{
+  const [courseTitle, setCourseTitle]=useState([])
+  // credit hour jog kortesi
+  const [creditHour, setCreditHour] = useState(0)
+  // for remaining credits hisab korbo
+  const [remainingCH, setRemainingCH] = useState(0);
+ 
+  
+// optional total price state 
+const [totalPrice, setTotalPrice] = useState(0);
+// 
 
-    // console.log('Adding',creditHour);
-    
-  // console.log('newcourseTitle',newcourseTitle)
-  // console.log(courseTitle,id);
+// onlcick function ta
+  const handleAddCourse = (id,coursetitle,credithour,price)=>{
+ 
   
   const ifExist = courseTitle.includes(coursetitle);
   // console.log(ifExist,'ifexist')
- 
-  if(ifExist) {
-       setIfExist(true)
-    //  alert('Already added to cart')
-  
-  }
-  else{
-    // const newcourseTitle=  Array.from(new Set([...courseTitle,coursetitle]))
-    const newcourseTitle=   [...courseTitle,coursetitle]
-    setCourseTitle(newcourseTitle)
-
-    const newCredithour = creditHour+credithour
-
-    if(newCredithour>20){
-      return alert('Sorry, course exceeds maximum credit limit (20hr).')
-      // setwarningforch(true)
-    }else{
-
-      setCreditHour(newCredithour);
-    }
-    }
-    // console.log("New creditHour:", newCredithour);
-    // setIfExist(true);
-
-    // Automatically hide the Alert after 3 seconds
-    setTimeout(() => {
+  if(ifExist ) {
+    setIfExist(true)
+     // Automatically hide the Alert after 3 seconds
+     setTimeout(() => {
       setIfExist(false);
     }, 3000);
+    //  alert('Already added to cart')
+    return;
+  }
+   else
+  {
+    const newCredithour = creditHour+credithour
+    
+    console.log('if kaj korlo',remainingCH)
+    if( newCredithour>20){
+     
+   // ===============================
+   setSwalProps({
+    show: true,
+    title: 'Sorry!',
+    // text: 'Course exceeds maximum credit limit (20hr).',
+    html: '<p class="bold  ">Course exceeds maximum credit limit (20hr).</p>',
+  });
+   // Automatically hide the Alert after 3 seconds
+   
+ return;
+}
+ 
+
+    console.log('card er credit hour total ',newCredithour)
+    // 
+    const newRemaining = 20-newCredithour;
+    // const newRemaining =remainingCH-credithour;
+
+    const newcourseTitle=   [...courseTitle,coursetitle]
+    // price total calculation here
+    const newprice = price+totalPrice;
+    // console.log('remaining koto credit thaklo dekha ',remainingCH)
+    setCreditHour(newCredithour);
+    setCourseTitle(newcourseTitle)   
+    setRemainingCH(newRemaining);
+    setTotalPrice(newprice)
+   
+    
+    
+  }
+    
+
     // console.log(newcourseTitle)
   }
+
   return (
     <><Header></Header>
     <div className='flex mx-auto'>
       <Cards      handleAddCourse={handleAddCourse}
             ></Cards>
-         
+             <SweetAlert2 {...swalProps} />
       <Carts 
-    ifExist={ifExist}  courseTitle={courseTitle} creditHour={creditHour}></Carts>
+    ifExist={ifExist}
+    courseTitle={courseTitle} 
+    creditHour={creditHour}
+    totalPrice={totalPrice}
+    remainingCH={remainingCH}
+    ></Carts>
+    
     </div></>
   )
 }
