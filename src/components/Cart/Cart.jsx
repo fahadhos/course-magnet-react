c4d8c5dfaa81063a345fba0257a49aa840e21282
fahadhos <generalus159@gmail.com>
2023-09-15 16:15:32 +0600

    Added: totalPrice state passed in  :4 and :24 line

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
  
 import Alert from '../Alert/Alert';
 import PropTypes from 'prop-types';
-const Cart = ({id,courseTitle,creditHour, ifExist}) => {
+const Cart = ({id,courseTitle,creditHour, ifExist,remainingCH,totalPrice}) => {
    
     return (
        <> 
@@ -9,8 +9,9 @@ const Cart = ({id,courseTitle,creditHour, ifExist}) => {
     {ifExist &&    <Alert ></Alert>} 
         <div className="card w-96 bg-base-100 shadow-xl  ">
         <div className="card-body space-y-5">
-          <h2 className="card-title border-b-2 text-[#2F80ED] font-bold leading-[5rem] ">Credit Hour Remaining 7 hr</h2>
-          <h2 className="card-title  ">Course Name</h2>
+          <h2 className="card-title border-b-2 text-[#2F80ED] font-bold leading-[5rem] ">
+            Credit Hour Remaining {remainingCH} hr</h2>
+          <h2 key={id} className="card-title  ">Course Name</h2>
           <p className="text-[#1c1b1b99] font-normal">
             
             
@@ -20,7 +21,7 @@ const Cart = ({id,courseTitle,creditHour, ifExist}) => {
           <div className="card-actions justify-end">
             <p className="border-t-2 border-b-2 font-medium leading-[3rem]"><small> Total Credit Hour : {creditHour} </small></p>
               </div>
-                  <p className=" font-semibold"> <small>Total Price : 48000 USD </small></p>
+                  <p className=" font-semibold"> <small>Total Price : {totalPrice} USD </small></p>
         
         </div>
       </div></> 
@@ -30,6 +31,8 @@ Cart.propTypes={
     id: PropTypes.number,
     courseTitle: PropTypes.string,
     creditHour: PropTypes.number,
-    ifExist:PropTypes.bool
+    ifExist:PropTypes.bool    ,
+    remainingCH: PropTypes.number,
+    totalPrice: PropTypes.number,
 }
 export default Cart;
