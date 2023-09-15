a2db96878a20d79a249263c1fbebd696b0fc3a6a
fahadhos <generalus159@gmail.com>
2023-09-15 11:47:00 +0600

    Added: Title added here as Navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="space-y-5 p-10 bg-gray-50">
+      <h3 className="text-center font-bold text-3xl  ">
+
+        Course Registration
+      </h3>
+
+    </header>
+  );
+};
+
+export default Header;
