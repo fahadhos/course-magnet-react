7fa06d651f76a37c51fdbf6aa405e53ae72a76b8
fahadhos <generalus159@gmail.com>
2023-09-15 11:45:44 +0600

    Added: Parent  Card has been  created, passed handleAddCourse function , used courses as state, fetched json from data, created grid layout for showing 3cards in row , dynamically created Card component using map

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,34 @@
+ 
+import { useEffect, useState } from 'react';
+import Card from './../Card/Card';
+
+import PropTypes from 'prop-types';
+const Cards = ({handleAddCourse}) => {
+
+    const [courses, setCourses] =useState([]);
+ 
+    useEffect(()=>{
+         fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setCourses(data))
+    },[]) 
+
+
+
+    return (
+        <div className="grid grid-cols-3 gap-2 mx-auto">
+               {
+                courses.map((courses,idx)=><Card key={idx}
+                courses={courses}
+                handleAddCourse={handleAddCourse}
+                ></Card>)
+            }
+           
+        </div>
+    );
+};
+
+Cards.propTypes={
+    handleAddCourse: PropTypes.func.isRequired,
+}
+export default Cards;
